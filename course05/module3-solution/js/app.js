1fe46b657565eb7a6b10db71746ae28e040166a6
IntelligentSense <josue.santamaria@possible.com>
2017-06-19 15:58:36 -0600

    Assignment 3 solution

--- course05/module3-solution/js/app.js
+++ course05/module3-solution/js/app.js
@@ -14,7 +14,8 @@
     }
   }
 
-  function MenuSearchService() {
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http) {
     var service = this;
 
     service.getMatchedMenuItems = function (searchTerm) {
