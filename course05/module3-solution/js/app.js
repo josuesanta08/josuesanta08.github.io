00ba15f2bf7faf651d99dac658ffcffc40905d9d
IntelligentSense <josue.santamaria@possible.com>
2017-06-19 16:39:10 -0600

    Assignment 3 solution

--- course05/module3-solution/js/app.js
+++ course05/module3-solution/js/app.js
@@ -3,15 +3,41 @@
 
   angular.module('NarrowItDownApp', [])
   .controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService);
+  .service('MenuSearchService', MenuSearchService)
+  .directive('itemsList', itemsList);
+
+  function ItemsList() {
+    var ddo = {
+      templateUrl: '../templates/itemsList.html',
+      scope: {
+        list: '<foundItems',
+        onRemove: '&'
+      },
+      controller: ItemsListDirectiveController,
+      controllerAs: 'list',
+      bindToController: true
+    };
+
+    return ddo;
+  }
+
+  function ItemsListDirectiveController() {
+    var list = this;
+  }
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
     var NarrowItDown = this;
 
+    var NarrowItDown.found = [];
+
     NarrowItDown.search = function() {
-      var found = MenuSearchService.getMatchedMenuItems(NarrowItDown.searchTerm);
+      NarrowItDown.found = MenuSearchService.getMatchedMenuItems(NarrowItDown.searchTerm);
     }
+
+    NarrowItDown.removeItem = function ($index) {
+      NarrowItDown.found.splice($index, 1);
+    };
   }
 
   MenuSearchService.$inject = ['$http'];
@@ -23,9 +49,10 @@
         url: "https://davids-restaurant.herokuapp.com/menu_items.json"
       }).then(function (result) {
         // process result and only keep items that match
-        var foundItems = result.data;
-
-        console.log(foundItems);
+        var foundItems = result.data.menu_items;
+        foundItems = foundItems.filter(function(menu_item) {
+          return menu_item.description.indexOf(searchTerm) !== -1
+        });
 
         // return processed items
         return foundItems;
